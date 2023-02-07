f5fab0e93c26a8205bf1ea9ff245d023a1bddf67
Shannu96 <39362211+Shannu96@users.noreply.github.com>
2023-02-07 11:50:03 -0800

    Python_Task1(4)

---
+++ Task1_#4thquestion.py
@@ -0,0 +1,8 @@
+#4 .Write a program that takes input from the user and prints it using both Python 2.x and Python 3.x Version.
+name= input("enter the name")
+
+#Python2.x [No need for parenthesis]
+print"Name ", name
+
+#Python3.x [Parenthesis required]
+print("Name" ,name)
